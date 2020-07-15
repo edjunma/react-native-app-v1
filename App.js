693e1fe2ed97408213e97a6ededc8a034c953848
Edmond Ma <edjunma@gmail.com>
2020-07-15 01:06:47 -0400

    finish todo app, still have more features to add

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/Header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
 	const [todos, setTodos] = useState([
@@ -9,13 +11,28 @@ export default function App() {
 		{ text: 'play on the switch', key: '3' },
 	]);
 
+	const pressHandler = (key) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter((todo) => todo.key != key);
+		});
+	};
+
+	const submitHandler = (text) => {
+		setTodos((prevTodos) => {
+			return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+		});
+	};
+
 	return (
 		<View style={styles.container}>
 			<Header />
 			<View style={styles.content}>
-				{/* To Do Form */}
+				<AddTodo submitHandler={submitHandler} />
 				<View style={styles.list}>
-					<FlatList data={todos} renderItem={({ item }) => <Text>{item.text}</Text>}></FlatList>
+					<FlatList
+						data={todos}
+						renderItem={({ item }) => <TodoItem item={item} pressHandler={pressHandler} />}
+					></FlatList>
 				</View>
 			</View>
 		</View>
